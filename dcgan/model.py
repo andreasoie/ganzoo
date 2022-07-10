added args, utils and fixed normalization

dcgan/model.py
@@ -1,10 +1,7 @@
-"""
-DCAN from scratch
-"""
-
 from typing import List, Tuple
 
 import torch
+from torch import nn
 
 
 def initialize_weights(
@@ -18,7 +15,7 @@ def initialize_weights(
             module.bias.data.zero_()
 
 
-def get_channel_sizes(image_shape: Tuple[int], reverse: bool = False) -> List[int]:
+def _get_channel_sizes(image_shape: Tuple[int], reverse: bool = False) -> List[int]:
     assert image_shape[1] == image_shape[2], "Image must be square"
     output_channels = [
         8 * image_shape[1],
@@ -33,11 +30,9 @@ def get_channel_sizes(image_shape: Tuple[int], reverse: bool = False) -> List[in
 
 
 class Generator(torch.nn.Module):
-    """Upsamples by uses fractionally-strided convolutions"""
-
     def __init__(self, latent_dimensions: int, image_shape: Tuple[int]) -> None:
         super().__init__()
-        self.output_channels = get_channel_sizes(image_shape)
+        self.output_channels = _get_channel_sizes(image_shape)
         self.conv1 = torch.nn.Sequential(
             torch.nn.ConvTranspose2d(
                 in_channels=latent_dimensions,
@@ -50,7 +45,6 @@ class Generator(torch.nn.Module):
             torch.nn.BatchNorm2d(num_features=self.output_channels[0]),
             torch.nn.ReLU(inplace=True),
         )
-        self.DEBUG = False
 
         self.conv2 = torch.nn.Sequential(
             torch.nn.ConvTranspose2d(
@@ -113,11 +107,9 @@ class Generator(torch.nn.Module):
 
 
 class Discriminator(torch.nn.Module):
-    """Downsampled by uses convolutions"""
-
     def __init__(self, image_shape: Tuple[int]) -> None:
         super().__init__()
-        self.output_channels = get_channel_sizes(image_shape, reverse=True)
+        self.output_channels = _get_channel_sizes(image_shape, reverse=True)
         self.conv1 = torch.nn.Sequential(
             torch.nn.Conv2d(
                 in_channels=self.output_channels[0],
@@ -179,7 +171,6 @@ class Discriminator(torch.nn.Module):
             ),
             torch.nn.Sigmoid(),
         )
-        self.DEBUG = False
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv1(x)
@@ -187,25 +178,5 @@ class Discriminator(torch.nn.Module):
         out = self.conv3(out)
         out = self.conv4(out)
         out = self.conv5(out)
-        return out.view(-1, 1).squeeze(1)  # (64, 1, 1, 1) to (64)
-
-
-class DCGAN(torch.nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pass
-
-
-if __name__ == "__main__":
-
-    BS = 64
-    z_dim = 100
-    image_shape = (1, 64, 64)
-
-    generator = Generator(latent_dimensions=z_dim, image_shape=image_shape)
-    discriminator = Discriminator(image_shape=image_shape)
-
-    initialize_weights(generator)
-    initialize_weights(discriminator)
+        out = out.view(-1, 1).squeeze(1)
+        return out

dcgan/train.py
@@ -1,119 +1,95 @@
+import argparse
 import os
-import sys
 
 import torch
 import torchvision.utils as vutils
-from matplotlib import pyplot as plt
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 from dcgan.model import Discriminator, Generator, initialize_weights
-from tools.utils import get_device, set_seeds
+from tools.data import load_mnist
+from tools.utils import get_device, init_outdir, set_seeds
 
 torch.backends.cudnn.benchmark = True
-set_seeds(1337)
+set_seeds(3407)
 
-BS = 1024
-IMAGE_SHAPE = (1, 64, 64)  # resize 28x28 to 64x64
-IMAGE_RESIZE = (64, 64)
+outdir = init_outdir(__file__, "out")
+parser = argparse.ArgumentParser()
+parser.add_argument("--bs", type=int, default=128)
+parser.add_argument("--epochs", type=int, default=8)
+parser.add_argument("--lr", type=float, default=0.0002)
+parser.add_argument("--zdim", type=int, default=100)
+args = parser.parse_args()
 
-tfms = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Resize(IMAGE_RESIZE),
-        transforms.Normalize((0.1307,), (0.3081,)),
-    ]
-)
-dataset = datasets.MNIST(root="./datasets", train=True, download=True, transform=tfms)
-dataloader = DataLoader(dataset, batch_size=BS, shuffle=True, num_workers=2)
+image_shape = 1, 64, 64  # upscaled
+dataloader = load_mnist(args.bs, image_shape)
+device = get_device()
 
-N_EPOCHS = 25
-Z_DIMENTION = 100
-LEARNING_RATE = 0.0002
-DEVICE = get_device()
+generator = Generator(args.zdim, image_shape)
+initialize_weights(generator)
+generator.to(device)
 
-modelG = Generator(latent_dimensions=Z_DIMENTION, image_shape=IMAGE_SHAPE)
-initialize_weights(modelG)
-modelG.to(DEVICE)
+discriminator = Discriminator(image_shape)
+initialize_weights(discriminator)
+discriminator.to(device)
 
-modelD = Discriminator(image_shape=IMAGE_SHAPE)
-initialize_weights(modelD)
-modelD.to(DEVICE)
-
-optimG = torch.optim.Adam(modelG.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-optimD = torch.optim.Adam(modelD.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
 criterion = torch.nn.BCELoss()
+optim_G = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(0.5, 0.999))
+optim_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
-BASE_DIR = os.path.join("dcgan", "outputs")
-CKT_DIR = os.path.join(BASE_DIR, "checkpoints")
-IMG_DIR = os.path.join(BASE_DIR, "images")
-
-os.makedirs(BASE_DIR, exist_ok=True)
-os.makedirs(CKT_DIR, exist_ok=True)
-os.makedirs(IMG_DIR, exist_ok=True)
-
-
-for epoch in range(N_EPOCHS):
-    gloss = []
-    dloss = []
-
+for epoch in range(args.epochs):
+    print("")
     for idx, batch in enumerate(dataloader):
-        real_images = batch[0].to(DEVICE)
-        n_labels = batch[1].shape[0]
+        real_images = batch[0].to(device)
+        batch_size = batch[0].shape[0]
 
-        # Discriminate with REAL
-        modelD.zero_grad()
-        true_labels = torch.ones((n_labels,), dtype=real_images.dtype, device=DEVICE)
-        logits = modelD(real_images)
-        lossD_real = criterion(logits, true_labels)
-        lossD_real.backward()
+        noise_shape = (batch_size, args.zdim, 1, 1)
+        noise = torch.randn(noise_shape, dtype=real_images.dtype, device=device)
+        true_labels = torch.ones((batch_size,), dtype=real_images.dtype, device=device)
+        fake_labels = torch.zeros((batch_size,), dtype=real_images.dtype, device=device)
+
+        # Discriminate on real images
+        discriminator.zero_grad()
+        logits = discriminator(real_images)
+        loss_real_D = criterion(logits, true_labels)
+        loss_real_D.backward()
         D_x = logits.mean().item()
 
-        # Discriminate with FAKE
-        noise_vector = torch.randn(n_labels, Z_DIMENTION, 1, 1, device=DEVICE)
-        fake_images = modelG(noise_vector)
-        fake_labels = torch.zeros((n_labels,), dtype=fake_images.dtype, device=DEVICE)
-        logits = modelD(fake_images.detach())
+        # Discriminate on fake images
+        fake_images = generator(noise)
+        logits = discriminator(fake_images.detach())
         lossD_fake = criterion(logits, fake_labels)
         lossD_fake.backward()
         D_G_z1 = logits.mean().item()
-        lossD = lossD_real + lossD_fake
-        optimD.step()
+        lossD = loss_real_D + lossD_fake
+        optim_D.step()
 
-        # Generate
-        modelG.zero_grad()
-        true_labels = torch.ones((n_labels,), dtype=real_images.dtype, device=DEVICE)
-        logits = modelD(fake_images)
+        # Generate fake images
+        generator.zero_grad()
+        logits = discriminator(fake_images)
         lossG = criterion(logits, true_labels)
         lossG.backward()
         D_G_z2 = logits.mean().item()
-        optimG.step()
+        optim_G.step()
 
-        gloss.append(lossG.item())
-        dloss.append(lossD.item())
-
-        print(
-            f"Epoch: {epoch}, Batch: {idx}/{len(dataloader)}, D_x: {round(D_x, 4)}, D_G_z1: {round(D_G_z1, 4)}, D_G_z2: {round(D_G_z2, 4)}, G_loss: {round(lossG.item(), 4)}, D_loss: {round(lossD.item(), 4)}"
-        )
-
-        if idx % 200 == 0:
-            test_noise = torch.randn(BS, Z_DIMENTION, 1, 1, device=DEVICE)
-            savedir_real = os.path.join(IMG_DIR, f"real_images.png")
-            savedir_fake = os.path.join(IMG_DIR, f"fake_images_{epoch}.png")
-            vutils.save_image(real_images, savedir_real, normalize=True)
-            fake = modelG(test_noise)
-            vutils.save_image(fake.detach(), savedir_fake, normalize=True)
-
-    plt.figure(figsize=(10, 5))
-    plt.title("Generator and Discriminator Loss")
-    plt.plot(gloss, label="Generator")
-    plt.plot(dloss, label="Discriminator")
-    plt.xlabel("Iterations")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.savefig(os.path.join(IMG_DIR, f"loss_{epoch}.png"))
-
-    gpath = os.path.join(CKT_DIR, f"modelG_epoch_{epoch}.pth")
-    dpath = os.path.join(CKT_DIR, f"modelD_epoch_{epoch}.pth")
-    torch.save(modelG.state_dict(), gpath)
-    torch.save(modelD.state_dict(), dpath)
+        if idx % (len(dataloader) // 10) == 0:
+            fill, pad = " ", 4
+            print(
+                f"Epoch: {epoch:{fill}{pad}}",
+                f"Batch: {idx:{fill}{pad}}/{len(dataloader)}",
+                f"D(x): {D_x:.03f}",
+                f"D(G(z1)): {D_G_z1:.03f}",
+                f"D(G(z2)): {D_G_z2:.03f}",
+                f"G_loss: {lossG:.03f}",
+                f"D_loss: {lossD:.03f}",
+                sep=" | ",
+            )
+    # ---
+    NUMBERS_TO_SHOW = 16
+    dummy_shape = (NUMBERS_TO_SHOW, args.zdim, 1, 1)
+    dummynoise = torch.randn(dummy_shape, dtype=real_images.dtype, device=device)
+    fake = generator(dummynoise)
+    path_real = os.path.join(outdir, f"real_0.png")
+    path_fake = os.path.join(outdir, f"fake_{epoch}.png")
+    vutils.save_image(fake.detach(), path_fake, normalize=True)
+    vutils.save_image(
+        real_images[0:NUMBERS_TO_SHOW, :, :, :], path_real, normalize=True
+    )

tools/utils.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import numpy as np
@@ -8,7 +9,13 @@ def get_device() -> torch.device:
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def set_seeds(lucky_number: int = 1337):
+def set_seeds(lucky_number: int = 3407):
     random.seed(lucky_number)
     np.random.seed(lucky_number)
     torch.manual_seed(lucky_number)
+
+
+def init_outdir(file_name: str, name: str) -> str:
+    outdir = os.path.join(os.path.dirname(os.path.abspath(file_name)), name)
+    os.makedirs(outdir, exist_ok=True)
+    return outdir